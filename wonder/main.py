e39836c5c9817b26d4954a39b6827e7d88b572b3
Tadahiro Yamamura <mt.village12@gmail.com>
2018-09-17 21:12:13 +0900

    create sub commands

--- wonder/main.py
+++ wonder/main.py
@@ -1,5 +1,21 @@
+import click
+
+
+@click.group(invoke_without_command=False)
 def wonder():
-  print('wonder!')
+  pass
+
+
+@wonder.command()
+def world():
+  click.echo('what a wonderful world!')
+
+
+@wonder.command()
+@click.argument('name')
+def beautiful(name):
+  click.echo('{} is beautiful!'.format(name))
+
 
 if __name__ == '__main__':
   wonder()
